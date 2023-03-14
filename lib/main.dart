e5cc2662515e34173de2191dc6d0348817a852a9
3stupidguys <jyp>
2023-03-14 15:44:28 +0900

    Login

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,14 @@ import 'screens/screen_lost.dart';
 import 'screens/screen_community.dart';
 import 'screens/screen_bus.dart';
 import 'screens/screen_notice.dart';
-
+import 'screens/screen_login.dart';
+import 'screens/screen_splash.dart';
+import 'screens/screen_register.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:exfirst/firebase_options.dart';
+import 'package:exfirst/models/model_auth.dart';
+import 'package:provider/provider.dart';
 
 // void main() => runApp(MaterialApp(
 //   debugShowCheckedModeBanner: false,
@@ -20,17 +27,32 @@ import 'screens/screen_notice.dart';
 //   }
 // }
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Demo',
+    return MultiProvider(
+        providers: [
+        ChangeNotifierProvider(create: (_) => FirebaseAuthProvider()),
+    ],
+    child: MaterialApp(
+      title: 'HYUPSUNG',
       theme: ThemeData(fontFamily: 'Roboto'),
-      home: HomePage(),
+      routes: {
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginScreen(),
+        '/splash': (context) => SplashScreen(),
+        '/register': (context) => RegisterScreen(),
+      },
+      initialRoute: '/splash',
+    ),
       );
 
   }
--- lib/screens/screen_home.dart
+++ lib/screens/screen_home.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'screen_community.dart';
 import 'screen_use.dart';
+import 'package:exfirst/models/model_auth.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -395,5 +398,22 @@ class PageView4 extends State<test4>{
 
 
     );
+  }
+}
+
+class LoginOutButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final authClient =
+    Provider.of<FirebaseAuthProvider>(context, listen: false);
+    return TextButton(
+        onPressed: () async {
+          await authClient.logout();
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(content: Text('logout!')));
+          Navigator.of(context).pushReplacementNamed('/login');
+        },
+        child: Text('logout'));
   }
 }
